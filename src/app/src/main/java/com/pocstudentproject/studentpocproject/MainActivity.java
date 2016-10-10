7314505cf048c503282c1768b5d86ad2f335ce35
renanroncarati <renanroncarati@gmail.com>
2016-10-10 01:20:18 -0300

    add layout new user with dialog and floating button

--- src/app/src/main/java/com/pocstudentproject/studentpocproject/MainActivity.java
+++ src/app/src/main/java/com/pocstudentproject/studentpocproject/MainActivity.java
@@ -1,7 +1,13 @@
 package com.pocstudentproject.studentpocproject;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.getbase.floatingactionbutton.FloatingActionButton;
@@ -42,7 +48,46 @@ public class MainActivity extends Activity {
 
     @Click(R.id.pink_icon)
     void floatAction() {
-        Toast.makeText(this, "Intent Tela Adicionar novo Student", Toast.LENGTH_SHORT).show();
+
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View view = inflater.inflate(R.layout.dialog_newuser, null);
+
+        //Dialog Elements
+        final EditText userName = (EditText) view.findViewById(R.id.edt_name);
+
+        final EditText userAge = (EditText) view.findViewById(R.id.edt_idade);
+
+        final EditText userAddress = (EditText) view.findViewById(R.id.edt_endereco);
+
+        final EditText userPhone = (EditText) view.findViewById(R.id.edt_telefone);
+
+        final EditText userPhotoUri = (EditText) view.findViewById(R.id.edt_foto);
+
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Novo Aluno");
+        builder.setView(view);
+        builder.setPositiveButton("Salvar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                //do not prevent null fields
+                createStudent(userName.getText().toString(), Integer.parseInt(userAge.getText().toString()), userPhotoUri.getText().toString()
+                        , userPhone.getText().toString(), userAddress.getText().toString());
+
+                Toast.makeText(getBaseContext(), "Aluno Criado", Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        builder.show();
+//        AlertDialog dialog = builder.create();
+//        dialog.show();
     }
 
     @UiThread
@@ -62,21 +107,53 @@ public class MainActivity extends Activity {
 //metodo delete
 //        serviceRest.removeStudent("AjoYF598sQ");
 
-        //metodo create new student
-//        Student newStudent = new Student() {
-//            {
-//                setEndereco("rua das oliveiras");
-//                setFotoUrl("https://robohash.org/1460852849489.png");
-//                setIdade(26);
-//                setNome("Marmita e marmota");
-//                setTelefone("3177777785252");
-//            }
-//        };
-//        serviceRest.createStudent(newStudent);
+
 
     }
 
     void deleteStudent(int position) {
 
     }
+
+    @Background
+    void createStudent(final String name, final int age, final String photoUri, final String phone, final String address) {
+        //metodo create new student
+        Student newStudent = new Student() {
+            {
+                setEndereco(address);
+                setFotoUrl(photoUri);
+                setIdade(age);
+                setNome(name);
+                setTelefone(phone);
+            }
+        };
+        serviceRest.createStudent(newStudent);
+
+        RequestStudentData();
+    }
+
+
+//    @Override
+//    public Dialog onCreateDialog(Bundle savedInstanceState) {
+//        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+//        // Get the layout inflater
+//        LayoutInflater inflater = getActivity().getLayoutInflater();
+//
+//        // Inflate and set the layout for the dialog
+//        // Pass null as the parent view because its going in the dialog layout
+//        builder.setView(inflater.inflate(R.layout.dialog_signin, null))
+//                // Add action buttons
+//                .setPositiveButton(R.string.signin, new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int id) {
+//                        // sign in the user ...
+//                    }
+//                })
+//                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+//                    public void onClick(DialogInterface dialog, int id) {
+//                        LoginDialogFragment.this.getDialog().cancel();
+//                    }
+//                });
+//        return builder.create();
+//    }
 }
